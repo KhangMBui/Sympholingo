61238e6c9ac8d33fe5081cfc9374043211b6c367
Donovan Tran <donovantran@Donovans-MacBook-Air.local>
2025-02-15 20:10:53 -0800

    Revert "Make things hidden"
    This reverts commit 21a5da0973b2e12290284ea80b7b3ecc8bbed48c.

--- backend/src/openAI/openaiService.ts
+++ backend/src/openAI/openaiService.ts
@@ -1,11 +1,10 @@
-import OpenAI from "openai"
-import dotenv from "dotenv";
+import OpenAI from "openai";
+//import dotenv from "dotenv";
 
-dotenv.config({ path: '../backend/src/config/.env' });
+//dotenv.config();
 
-console.log("OpenAI API Key:", process.env.OPENAI_API_KEY);
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: "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"
 });
 
 interface TranslationResult {
